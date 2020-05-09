70bc8011d856540c8b18cc113bb414b8272d72a6
c17an <kimchanmin1@gmail.com>
2020-05-09 17:21:33 +0900

    added views

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # post_list 라는 뷰가 루트 URL에 할당됨. (메인페이지)
     path('', views.post_list, name='post_list'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 ]
